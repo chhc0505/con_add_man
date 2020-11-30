bdedf7e9c835342d61929a0aba2f1c4a0920fc5b
jinseok05 <d010505@naver.com>
2020-11-30 22:58:10 +0900

    con_main appended

--- con_main.c
+++ con_main.c
@@ -1,37 +1,29 @@
 #include<stdio.h>
+#include<string.h>
+struct student{
+	char name[10];
+	int score;
+};
+
 
 int who_are_you();
-
-
-
+void base_pr(struct student *, int);
+void num_sequence_pr(struct student [], int);
 int main()
 {
     int signal = 1;
     int choose;
     int choose2;
-
-
-    struct student {
-        char name;
-        int score;
-    };
-
-    const char *start_menu[] = {
-        "1.학생 점수 출력",
-        "2.평균 점수",
-        "3.프로그램 종료",
-    };
-
     
 
-    if(who_are_you == 1)
-        printf("접속 완료!!\n");
+    if(who_are_you() == 1)
+        printf("접속 완료!!\n\n\n");
     else return 0;
 
-    char class_name;
+    char class_name[10];
     int class_num;
     printf("반 이름:");
-    scanf("%s",&class_name);
+    scanf("%s", class_name);
 
     printf("총 명수:");
     scanf("%d",&class_num);
@@ -39,13 +31,13 @@ int main()
     struct student slist[class_num];
     for(int i = 0;  i< class_num; i++){
         printf("%d번 학생 이름:",i+1);
-        scanf("%s",&slist[i].name);
+        scanf("%s",slist[i].name);
 
         printf("%d번 학생 점수:",i+1);
         scanf("%d",&slist[i].score);  
     }
 
-    printf("%s반 %d명 저장 완료!",class_name,class_num);
+    printf("%s반 %d명 저장 완료!\n\n\n", class_name, class_num);
 
 
 
@@ -58,44 +50,44 @@ int main()
 
 
     while(signal){
-        int i;
-        for (i=0; i<4; i++){
-            printf("%s\n" , start_menu[i]);
-        }
 
-        printf("1 / 2 / 3 / 4 입력:");
+        printf("1.출력\n");
+		printf("2.평균 구하기\n");
+		printf("3.프로그램 종료!\n");
+        printf("1 / 2 / 3 입력:");
         scanf("%d" , &choose);
+		printf("\n\n");
 
         switch(choose){
             case 1:
-                printf("1.출력\n2.점수 순서대로 출력\n3.이름 순서대로 출력");
-                scnaf("%d", &choose2);
-
+                printf("1.기본출력\n2.점수 순서대로 출력\n3.이름 순서대로 출력\n");
+				printf("1 / 2 / 3 입력:");
+                scanf("%d", &choose2);
+				printf("\n\n\n");
                 switch(choose2){
                     case 1:
-                        printf("출력 실행!\n");
-                        print_1(slist,class_num);
+                        printf("기본출력 실행!\n");
+                        base_pr(slist,class_num);
                         break;
                     case 2:
                         printf("점수 순서대로 출력 실행! \n");
+						num_sequence_pr(slist,class_num);
                         break;
                     case 3:
                         printf("이름 순서대로 출력 실행! \n");
                         break;
                 }
-
-
                 break;
             case 2:
-                printf("2번 실행!");
+                printf("2번 실행!\n");
                 break;
             case 3:
-                printf("3번 실행!");
-                printf("프로그램 종료!");
+                printf("3번 실행!\n");
+                printf("프로그램 종료!\n");
                 signal = 0;
                 break;
             default:
-                printf("잘못 입력하셨습니다.");
+                printf("잘못 입력하셨습니다.\n");
         }
 
     
@@ -110,12 +102,13 @@ int who_are_you(void)
         int pass;    
     };
 
-    struct user master = {"Jinseok05",0505};
+    struct user master = {"J" ,5555};
     struct user guest;
 
     printf("사용자 이름 입력:");
-    scanf("%s",&guest.name);
-    if(master.name != guest.name){
+    printf("%s", master.name);
+	scanf("%s",guest.name);
+    if(master.name[0] != guest.name[0]){
         printf("이름 불일치!\n");
         printf("프로그램을 종료합니다!\n");
         return 0;
@@ -133,15 +126,62 @@ int who_are_you(void)
     return 1;
 }
 
-void print_1(struct student **slist, int size)
+void base_pr(struct student *slist, int size)
 {
+	printf("-------------------------------------\n");
     for(int i = 0; i < size; i++){
-        printf("%d. 이름: %s, 점수: %d\n",i+1, slist[i]->name,  slist[i]->score);
+        printf("%d. 이름: %s, 점수: %d\n",i+1, (slist+i)->name, (slist+i)->score);
     }
+	printf("-------------------------------------\n");
 
 
 
 }
+
+void num_sequence_pr(struct student slist[], int size){
+	
+	struct student prlist[size];
+	int maxi;
+	char temp_ch[10];
+	int temp_in;	
+
+	for(int i = 0; i < size -1; i++){
+		maxi = i;
+		for(int j = i+1; j < size; j++){
+			if (slist[j].score > slist[maxi].score){
+				maxi = j;
+			}
+		
+
+		
+		strcpy(temp_ch, slist[i].name);
+		temp_in = slist[i].score;
+
+		strcpy(slist[i].name, slist[maxi].name);
+		slist[i].score = slist[maxi].score;
+
+		strcpy(slist[maxi].name, temp_ch);
+		slist[maxi].score = temp_in;
+		}
+	}
+	
+
+	printf("--------------------------------------\n");
+	for(int k = 0; k < size; k++){
+		printf("%d.이름: %s   점수: %d\n",k+1,slist[k].name,slist[k].score); 
+
+	}
+	printf("--------------------------------------\n");
+}
+
+
+
+
+
+
+
+
+
 
 
 
